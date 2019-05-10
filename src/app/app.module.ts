bf361324ce0d7512a6d355df093418e6bf807d4b
Mohammad-Quanit <muhammadquanit@gmail.com>
2019-05-11 00:32:00 +0500

    NGXS Reactive Forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,24 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { NgxsModule } from "@ngxs/store";
+import { NgxsReduxDevtoolsPluginModule } from "@ngxs/devtools-plugin";
+import { NgxsLoggerPluginModule } from "@ngxs/logger-plugin";
+import { AddEmployeeComponent } from './components/add-employee/add-employee.component';
+import { ViewEmployeeComponent } from './components/view-employee/view-employee.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddEmployeeComponent,
+    ViewEmployeeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxsModule.forRoot(),
+    NgxsReduxDevtoolsPluginModule,
+    NgxsLoggerPluginModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
